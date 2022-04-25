c2788f5e087bc94bd42901c6035093d44621d9b5
Serhii Kushnerenko <skushnerenko@corelogic.com>
2022-04-25 20:49:14 +0300

    Task 4 fix

--- src/app/components/products/service/products.service.ts
+++ src/app/components/products/service/products.service.ts
@@ -2,27 +2,22 @@ import {Injectable} from '@angular/core';
 import {CategoryEnum} from '../../first/category.enum';
 import {Product} from '../model/product';
 
+const productArray = [
+  new Product( 'tea', 'description1', 5, CategoryEnum.FOOD, true),
+  new Product('map', 'description2', 120, CategoryEnum.BOOKS, false),
+  new Product('phone', 'description3', 1200, CategoryEnum.ELECTRONICS, true)
+];
+
+const productArrayPromise = Promise.resolve(productArray);
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
-  private productArray: Product[] =  [
-    new Product('tea', 'description1', 5, CategoryEnum.FOOD, true),
-    new Product('map', 'description2', 120, CategoryEnum.BOOKS, false),
-    new Product('phone', 'description3', 1200, CategoryEnum.ELECTRONICS, true)
-  ];
-
-  private products: Promise<Product>[]  =  this.productArray.map((product) => {
-    return new Promise<Product>((resolve, reject) => {
-      setTimeout(() => {
-        resolve(product);
-      }, 500);
-    });
-  });
-
 
 
   getProducts(): Promise<Product[]> {
-    return Promise.all(this.products) ;
+    return productArrayPromise;
   }
 }
